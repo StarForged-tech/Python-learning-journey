63501af82701718276711ebf5cdbea0adfa9cd3e
EKANSH KUMAR <231542553+StarForged-tech@users.noreply.github.com>
2026-05-15 12:11:30 +0530

    Added user input basics in Python

---
+++ day3_user_input.py
@@ -0,0 +1,14 @@
+# input() takes data from the user
+# print() displays output
+# \n creates a new line
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+goal = input("What do you want to become? ")
+
+print("\n--- USER INFORMATION ---")
+print("Name:", name)
+print("Age:", age)
+print("Goal:", goal)
+
+print("\nKeep learning and building consistently,", name + "!")
