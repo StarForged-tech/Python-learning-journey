58b7353e3f7dbcc53661bf3cdf87e476670b710f
EKANSH KUMAR <231542553+StarForged-tech@users.noreply.github.com>
2026-05-15 12:22:07 +0530

    Added Python type conversion and addition program

---
+++ day4_type_conversion.py
@@ -0,0 +1,16 @@
+# Type conversion changes one data type into another
+# int() converts to integer
+# float() converts to decimal number
+# str() converts to string
+# bool() converts to boolean
+
+age = int(input("Enter your age: ")) # here int() converts string to integer
+future_age = age + 5
+print("Your age after 5 years will be:", future_age)
+
+
+#Ask user to input 2 numbers, add them and print the result
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+sum = num_1 + num_2
+print("Sum is",sum)
