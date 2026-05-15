e7bd0bd1bbea1f3de812c095ba14636ba3f90ccf
EKANSH KUMAR <231542553+StarForged-tech@users.noreply.github.com>
2026-05-15 12:08:33 +0530

    Added Python variables and data types basics

---
+++ day2_variables.py
@@ -0,0 +1,16 @@
+name = "Ekansh Kumar"
+college = "Pimpri Chinchwad University"
+age = 18
+cgpa = 8.15
+is_learning_python = True
+
+print("Name:", name)
+print("College:", college)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Learning Python:", is_learning_python)
+
+# String stores text
+# Integer stores whole numbers
+# Float stores decimal numbers
+# Boolean stores True or False values
