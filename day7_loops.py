3210dc5dace78dfae5cf4d936be5b5503e8d0577
EKANSH KUMAR <231542553+StarForged-tech@users.noreply.github.com>
2026-05-18 22:15:46 +0530

    Added Python for loops and repetition basics
    Add explanations and examples for using loops in Python.

---
+++ day7_loops.py
@@ -0,0 +1,10 @@
+#Loops are used to repeat the tasks efficiently
+#In simple words loops helps us reduce the time and effort to repeat a code repeatedly in a program
+
+#This is for loop
+for i in range(5):
+    print("I am learning Python")
+
+#for has 3 conditions (Start, Stop, Step) where Stop must be specified
+for i in range(1, 6):
+    print("Round:", i)
