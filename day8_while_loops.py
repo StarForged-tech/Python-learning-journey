030a54b568901f6c7d6d0a93c2f7197a9dc24644
EKANSH KUMAR <231542553+StarForged-tech@users.noreply.github.com>
2026-05-18 22:32:53 +0530

    Added while loops with structured comments

---
+++ day8_while_loops.py
@@ -0,0 +1,26 @@
+# Learning repetition of program using while loop
+
+# Starting value
+count = 1
+
+# Loop runs while condition is True till Stop value
+while count <= 5: 
+    print("Count:", count)
+
+    # Increasing count after each iteration/step
+    count += 1 #Step value
+
+print("Loop finished")
+
+''' 
+    •for loop is used when repetitions are known.
+
+    •while loop is used when condition controls repetition.
+'''
+
+#Program to print numbers from 10 to 1 using while loop
+num = 10 #Start
+while num >= 1: #Stop
+  print(num)
+  num -= 1 #Step
+print("loop finished")
